8def92acbb000b7a9f79febd51b8c24c19e40fab
Naufallabibb <88694667+Naufallabibb@users.noreply.github.com>
2022-04-13 22:44:16 +0700

    Update Studi Kasus 4.cpp

--- Studi Kasus 4.cpp
+++ Studi Kasus 4.cpp
@@ -4,32 +4,34 @@ using namespace std;
 class Fibonacci {
   
 public:
-    long ubah1(int);//rekursif
-    long ubah2(int);//iteratif
-
+    long ubah(int n);
+    long ubah2(int n);
 private:
+	int i;
 	int a, b, c;
 };
   
-long Fibonacci::ubah1(int n)
+long Fibonacci::ubah(int n)
 {
-    long a=1, b=1, c;
-
+    a=1;
+    b=1;
+  
     cout << a << " " << b;
   
     for (int i=1; i<=n-2; i++) {
         c= a+b;
+        cout<< " "<<c;
+  
         a=b;
         b=c;
-        cout<<" "<<c<<" ";
     }
 }
 long Fibonacci::ubah2(int n){
-     if ((n==0)||(n==1)){
-   		return (1);
-  }
+ 	if((n==0)||(n==1)){
+	return (1);
+   	}
    else{
-   	   return (ubah2(n-1)+ubah2(n-2));
+   		return (ubah2(n-1)+ubah2(n-2));
    }
 }
   
@@ -38,8 +40,8 @@ int main()
     int n=11;
   
     Fibonacci fbc;
-	fbc.ubah1(n);//iteratif
+    fbc.ubah(n);
     cout<<endl;
-    fbc.ubah2(11);//rekursif
+    fbc.ubah2(11);
     return 0;
 }
